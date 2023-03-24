26a8d5a043054a293fa6ce1bc04b705cd7c7abad
Syed Daniyal Ahmed <syeddani23.99@gmail.com>
2023-03-24 19:19:42 +0500

    Post Method Problem

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,58 @@
 const express = require('express');
 const router = express.Router();
 
+//Parsing information coming from client
+router.get('/new', (req, res) => {
+  res.render('user/new');
+});
+
+router.post('/', (req, res) => {
+  res.send('hi');  
+});
+
+module.exports = router;
+
 
 /////// Router
-router.get('/', (req, res) => {
-  res.send('User Page');
-});
+// router.get('/', (req, res) => {
+//   res.send('User Page');
+// });
 
-router.get('/default', (req, res) => {
-  res.send('User Default Page');
-});
+// router.get('/default', (req, res) => {
+//   res.send('User Default Page');
+// });
 
-// Making Dynamic Router, For different URL.
-router.get('/all/:id', (req, res) => {
-  let data = req.params.id; //Requset the id givne in url. It should be same as the URL path
-  res.send(`Get User with ID${data}`);
-});
+// // Making Dynamic Router, For different URL.
+// router.get('/all/:id', (req, res) => {
+//   let data = req.params.id; //Requset the id givne in url. It should be same as the URL path
+//   res.send(`Get User with ID${data}`);
+// });
 
-// get, put and delete pattern is very common, so express created another method
-// callled route. This method allow us to stack all three in same function.
+// // get, put and delete pattern is very common, so express created another method
+// // callled route. This method allow us to stack all three in same function.
 
-router
-  .route('/new/:id')
-  .get((req, res) => {
-    let data = req.params.id;
-    res.send(`Get User with ID${data}`);
-  })
-  .put((req, res) => {
-    let data = req.params.id;
-    res.send(`Update User with ID${data}`);
-  })
-  .delete((req, res) => {
-    let data = req.params.id;
-    res.send(`Delete User with ID${data}`);
-  });
+// router
+//   .route('/new/:id')
+//   .get((req, res) => {
+//     let data = req.params.id;
+//     res.send(`Get User with ID${data}`);
+//   })
+//   .put((req, res) => {
+//     let data = req.params.id;
+//     res.send(`Update User with ID${data}`);
+//   })
+//   .delete((req, res) => {
+//     let data = req.params.id;
+//     res.send(`Delete User with ID${data}`);
+//   });
 
-const namesUser = [{firstName:"Daniyal" , lastName:"Ahmed"}]
+// const namesUser = [{firstName:"Daniyal" , lastName:"Ahmed"}]
 
-//next function is, if param function is called run the NEXT thing in line.
-  // This function will run everytime it finds the parameter in the url.
-  router.param("id",(req,res,next,id)=>{
-    console.log(id);
-    next() //Will go to the next piece of code that will be executed in the given url.  
-  })
+// //next function is, if param function is called run the NEXT thing in line.
+//   // This function will run everytime it finds the parameter in the url.
+//   router.param("id",(req,res,next,id)=>{
+//     console.log(id);
+//     next() //Will go to the next piece of code that will be executed in the given url.
+//   })
 
 
-  
-module.exports = router;
