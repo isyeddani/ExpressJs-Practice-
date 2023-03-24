e4579f0a73c1829dfb822a12b8d6e0c03f542e0c
Syed Daniyal Ahmed <syeddani23.99@gmail.com>
2023-03-24 15:19:04 +0500

    Express Tutorial About to End

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-const express = require ('express')
+const express = require('express');
 const app = express();
 
 //Telling application to use View Engine
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
+// app.use(logger);
+// First parameter is root path
+// Second parameter is the function, in function
+// Request, Response, next
+app.get("/",(req,res)=>{
+    console.log("Here")
+    // res.send("Hello World!") // will not use very offten just to for checking purpose.
+    // res.sendStatus(500) // Send to display error, (check:HTTP status code) .
+    // res.status(500).send("Hi")
+    // res.status(500).json({message:"Error"}) //Use to send to complete json code to page.
+    res.download('file.txt') // Allow user to download file.
 
-// // First parameter is root path
-// // Second parameter is the function, in function
-// // Request, Response, next
-// app.get("/",(req,res)=>{
-//     console.log("Here")
-//     // res.send("Hello World!") // will not use very offten just to for checking purpose.
-//     // res.sendStatus(500) // Send to display error, (check:HTTP status code) .
-//     // res.status(500).send("Hi")
-//     // res.status(500).json({message:"Error"}) //Use to send to complete json code to page.
-//     res.download('file.txt') // Allow user to download file. 
-    
-    
-//     //Use to render the page using the html file.
-//     // res.render("index") // For this view engine is required. To install library npm i ejs another(popular) is pug
-//     // second parameter will render the data send.
-//     res.render ("index", {data:"Daniyal"}) 
-// } )
+    //Use to render the page using the html file.
+    // res.render("index") // For this view engine is required. To install library npm i ejs another(popular) is pug
+    // second parameter will render the data send.
+    res.render ("index", {data:"Daniyal"})
+} )
 
-const userRouter = require("./routes/users")  // Import the router just created
-app.use("/user",userRouter) 
+
+// Serving out static files.
+// Using Middleware.
+app.use(express.static('public')); // express.static() takes the folder in which the render file is
+// It will render all files inside public folder.
+
+const userRouter = require('./routes/users'); // Import the router just created
+app.use('/user', userRouter);
+
+// Middleware Function 
+// Every Middleware function takes a request response and next
+function logger(req, res, next) {
+  console.log(req.originalUrl); // will just display the URl of the request
+  next();
+}
+
+app.get("/dan",logger,(req, res) => {
+    console.log("Logger Function")
+    res.send("Logger Function")
+})
 
 app.listen(3000);
-    
